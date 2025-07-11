58c5a14dc09111cfa05ac91cdb0b27c3ea3976e4
djamit849 <djamit849@gmail.com>
2025-07-11 20:31:38 +0530

    rg-amit file added

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "abhishek-rg"
   location = "East US"
 }
+resource "azurerm_resource_group" "rg1" {
+  name     = "amit-rg"
+  location = "East US"
+}
